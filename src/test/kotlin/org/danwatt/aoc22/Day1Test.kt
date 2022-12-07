84775b3adb4a9fba68ef88e36145554c7fca5e9d
Dan Watt <danwatt@gmail.com>
2022-12-06 19:19:20 -0600

    Add test case for day 1

--- src/test/kotlin/org/danwatt/aoc22/Day1Test.kt
+++ src/test/kotlin/org/danwatt/aoc22/Day1Test.kt
@@ -6,10 +6,37 @@ import org.junit.jupiter.api.Test
 class Day1Test {
     val input = loadLines("day1.txt")
     val day = Day1()
+    val testLines = """
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
+    """.trimIndent().lines()
+
+    @Test
+    fun testCase1() {
+        assertThat(day.part1(testLines)).isEqualTo(24000)
+    }
 
     @Test
     fun part1() {
         assertThat(day.part1(input)).isEqualTo(70369)
+    }
+
+    @Test
+    fun testCase2() {
+        assertThat(day.part2(testLines)).isEqualTo(45000)
+
     }
 
     @Test
